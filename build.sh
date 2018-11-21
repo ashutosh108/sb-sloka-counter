1d4f406ae906b23cf073ae8a9a428c2e42dda39a
Anton Voloshin <ashutosh108@gmail.com>
2018-11-21 16:03:02 +0200

    build scripts for dockerized Ubuntu 18.04 LTS

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # try build on various platforms and compilers using docker
 
-# ubuntu 14.04 (LTS 2014-2019), CMake 2.8.12.2, g++ 4.8.4
 for image in \
 	ashutosh108/dev:ubuntu14.04-cmake2.8.12.2-gcc4.8.4 \
-	ashutosh108/dev:ubuntu16.04-cmake3.5.1-gcc5.4.0; do
+	ashutosh108/dev:ubuntu16.04-cmake3.5.1-gcc5.4.0 \
+	ashutosh108/dev:ubuntu18.04-cmake3.10.2-gcc7.3.0 \
+	; do
 
 	docker run --rm -v //c/Users/ashutosh/src:/home/dev/src:ro --tmpfs /home/dev/build:exec "$image" su - dev -c "cd build && cmake ../src/sb-sloka-counter && cmake --build ."
 done
