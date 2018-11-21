aefc7710f0198e19aee8f0f7e5bf0d57e6662733
Anton Voloshin <ashutosh108@gmail.com>
2018-11-21 16:31:44 +0200

    add compilation script for dockerized centos7

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ for image in \
 	ashutosh108/dev:ubuntu14.04-cmake2.8.12.2-gcc4.8.4 \
 	ashutosh108/dev:ubuntu16.04-cmake3.5.1-gcc5.4.0 \
 	ashutosh108/dev:ubuntu18.04-cmake3.10.2-gcc7.3.0 \
+	ashutosh108/dev:centos7-cmake2.8.12.2-gcc4.8.5 \
 	; do
 
 	docker run --rm -v //c/Users/ashutosh/src:/home/dev/src:ro --tmpfs /home/dev/build:exec "$image" su - dev -c "cd build && cmake ../src/sb-sloka-counter && cmake --build ."
