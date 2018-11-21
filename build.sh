facfff15b0869ce004b72df11f88e0746a3bc455
Anton Voloshin <ashutosh108@gmail.com>
2018-11-21 15:13:16 +0200

    add script for compilation under ubuntu16.04

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 # try build on various platforms and compilers using docker
 
 # ubuntu 14.04 (LTS 2014-2019), CMake 2.8.12.2, g++ 4.8.4
-docker run --rm -v //c/Users/ashutosh/src:/home/dev/src:ro --tmpfs /home/dev/build:exec ashutosh108/dev:cmake2.8-gcc4.8 su - dev -c "cd build && cmake ../src/sb-sloka-counter && cmake --build ."
+for image in \
+	ashutosh108/dev:ubuntu14.04-cmake2.8.12.2-gcc4.8.4 \
+	ashutosh108/dev:ubuntu16.04-cmake3.5.1-gcc5.4.0; do
+
+	docker run --rm -v //c/Users/ashutosh/src:/home/dev/src:ro --tmpfs /home/dev/build:exec "$image" su - dev -c "cd build && cmake ../src/sb-sloka-counter && cmake --build ."
+done
